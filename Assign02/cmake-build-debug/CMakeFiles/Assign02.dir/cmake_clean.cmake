099c32aef7039ae4a15488dc5ccdfd9d7493e2a8
Ian <ian@wolvesinexile.com>
2021-09-15 10:25:27 -0500

    assign 01 done created assign 02

---
+++ Assign02/cmake-build-debug/CMakeFiles/Assign02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Assign02.exe"
+  "Assign02.pdb"
+  "CMakeFiles/Assign02.dir/Assign02.cpp.o"
+  "CMakeFiles/Assign02.dir/IntSet.cpp.o"
+  "libAssign02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assign02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
